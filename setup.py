62dc3b473ab4aac5928a093cf40cbdd0f3158023
DipuYadav5253 <dipuyadav5253@gmail.com>
2024-01-02 22:27:43 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup 
+
+setup(
+    name='wasteDetection',
+    version='0.0.0',
+    author='Dipu Yadav',
+    author_email='dipuyadav5253@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
